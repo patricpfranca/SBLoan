422338977a977af11aa698ac1f92d88fde741c85
Patric Pereira <patric.franca@petanjo.com>
2026-09-23 08:46:43 -0300

    Initial commit

---
+++ Example/SBLoan/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SBLoan
+//
+//  Created by Patric Pereira on 09/23/2026.
+//  Copyright (c) 2026 Patric Pereira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
