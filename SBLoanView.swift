d940bd1dddfa4c9732ec69b6ee85b76d545e7810
Patric Pereira <patric.franca@petanjo.com>
2026-09-25 07:56:13 -0300

    Enhance SBLoanView with UI components and layout
    - Added background color and gradient to the SBLoanView.
    - Implemented a header view with titles and a subtitle for better user experience.
    - Updated iOS deployment target to 13.0 in project settings for compatibility.

--- SBLoanView.swift
+++ SBLoanView.swift
@@ -9,10 +9,52 @@ import SwiftUI
 
 public struct SBLoanView: View {
     
+    // MARK: - UI Components
+    
+    private let backgroundColor = Color(red: 240.0/255.0, green: 240.0/255.0, blue: 240.0/255.0)
+    private let yellowColor = Color(red: 255.0/255.0, green: 186.0/255.0, blue: 5.0/255.0)
+    
+    // MARK: - Init
+    
     public init() {  }
     
+    // MARK: - View
+    
     public var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .top) {
+            LinearGradient(gradient: Gradient(colors: [yellowColor, backgroundColor]), startPoint: .top, endPoint: .bottom)
+            
+            VStack {
+                headerView
+                    .padding(.leading, 20)
+            }
+        }
+    }
+    
+    var headerView: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Empréstimo")
+                    .font(.system(size: 24))
+                    .foregroundColor(.black)
+                    .fontWeight(.light)
+                
+                Text("SwiftBank")
+                    .font(.system(size: 24))
+                    .foregroundColor(.black)
+                    .fontWeight(.light)
+                
+                Spacer()
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.top)
+            .padding(.bottom, 3)
+            
+            Text("Antecipe seus planos!")
+                .font(.system(size: 24))
+                .foregroundColor(.black)
+                .fontWeight(.light)
+        }
     }
 }
 
